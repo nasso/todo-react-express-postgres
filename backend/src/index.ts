9abf25265f50b8331349c9295a91951a6b066639
nasso <nassomails@gmail.com>
2023-03-13 09:21:51 +0000

    Add todo API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,54 @@ const SERVER_PORT = 8080;
 async function main() {
   const app = express();
 
-  // REST API
+  app.use(express.json());
+
+  // Returns all todos
+  // Example: curl http://localhost:8080/todos
   app.get("/todos", async (_req, res) => {
     const todos = await db.todo.findMany();
 
     res.json(todos);
+  });
+
+  // Adds a new todo
+  // Example: curl -X POST -H "Content-Type: application/json" -d '{"title": "Buy milk"}' http://localhost:8080/todos
+  app.post("/todos", async (req, res) => {
+    const todo = await db.todo.create({
+      data: {
+        title: req.body.title,
+        completed: false,
+      },
+    });
+
+    res.status(201).json(todo);
+  });
+
+  // Updates a todo
+  // Example: curl -X PUT -H "Content-Type: application/json" -d '{"completed": true}' http://localhost:8080/todos/1
+  app.put("/todos/:id", async (req, res) => {
+    const todo = await db.todo.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        completed: req.body.completed,
+      },
+    });
+
+    res.json(todo);
+  });
+
+  // Deletes a todo
+  // Example: curl -X DELETE http://localhost:8080/todos/1
+  app.delete("/todos/:id", async (req, res) => {
+    const todo = await db.todo.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    res.status(204).json(todo);
   });
 
   app.listen(SERVER_PORT, () => {
