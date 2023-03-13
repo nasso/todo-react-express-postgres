386c414916a8e59dc8174f64c4bde8fee33ce6ee
nasso <nassomails@gmail.com>
2023-03-13 06:59:12 +0000

    Add basic client-side todo list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,102 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const newId = (() => {
+  let id = 0;
+
+  return () => {
+    id += 1;
+    return id;
+  };
+})();
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState("");
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: newId(),
+        title,
+        completed: false,
+      },
+    ]);
+
+    setTitle("");
+  }
+
+  function updateTodo(id: number, completed: boolean) {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  }
+
+  function deleteTodo(id: number) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(newTodos);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex flex-col gap-4 pb-4 xl:items-center">
+      <header className="rounded-b-2xl bg-slate-800 xl:w-2/3">
+        <h1 className="p-4 text-center text-4xl font-bold">Todo List</h1>
+      </header>
+      <main className="flex w-screen flex-col justify-center gap-4 px-8 xl:w-2/3 xl:p-0">
+        <form
+          className="flex justify-center gap-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addTodo();
+          }}
+        >
+          <input
+            type="text"
+            className="h-10 grow rounded-lg bg-slate-800 px-4"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button className="h-10 rounded-lg bg-blue-500 px-4 text-white">
+            Add
+          </button>
+        </form>
+        <ul className="flex flex-col gap-4">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center gap-4 rounded-lg bg-slate-800 p-4"
+            >
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => updateTodo(todo.id, e.target.checked)}
+              />
+              <p className="grow">{todo.title}</p>
+              <button
+                className="h-10 rounded-md bg-red-500 px-4 text-white"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
